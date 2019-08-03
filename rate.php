40043338aa62da3d050ca5efb6dc116080cd5adf
Danil <waisee7@gmail.com>
2019-08-04 01:08:25 +0300

    - homework 4 with traits

--- rate.php
+++ rate.php
@@ -1,15 +1,35 @@
 <?php
 
-interface iRate{
-    public function checkAge();
+interface RateInterface{
 
-    public function getPrice();
+    public function checkAge($driverAge);
+
+    public function getPrice(int $km, int $minute, int $driverAge);
 }
 
-abstract class Rate
+trait Gps
+{
+    public function addGps($result, $minute)
+    {
+        $hours = intdiv($minute, 60) + 1;
+        $result = $result + ($hours * 15);
+        return $result;
+    }
+}
+
+trait Driver
+{
+    public function addDriver($result)
+    {
+        $result+=100;
+        return $result;
+    }
+}
+
+abstract class Rate implements RateInterface
 {
 
-    protected function checkAge($driverAge)
+    public function checkAge($driverAge)
     {
         if($driverAge < 18){
             echo 'Вы слишком молоды';
@@ -33,11 +53,20 @@ class Base extends Rate
     public $perKm = 10;
     public $perMinute = 3;
 
-    public function getPrice(int $km, int $minute, int $driverAge)
+    use Gps;
+
+    public function getPrice(int $km, int $minute, int $driverAge, bool $gps = false)
     {
+
         $coefficent = $this->checkAge($driverAge);
 
-        return ($this->perKm * $km + $this->perMinute * $minute) * $coefficent;
+        $result =  ($this->perKm * $km + $this->perMinute * $minute) * $coefficent;
+
+        if($gps){
+            $result = $this->addGps($result, $minute);
+        }
+
+        return $result;
     }
 }
 class Hour extends Rate
@@ -45,13 +74,26 @@ class Hour extends Rate
     public $perKm = 0;
     public $perHour = 200;
 
-    public function getPrice(int $km, int $minute, int $driverAge)
+    use Gps;
+    use Driver;
+
+    public function getPrice(int $km, int $minute, int $driverAge, bool $gps = false, bool $driver = false)
     {
         $hours = intdiv($minute, 60) + 1;
 
         $coefficent = $this->checkAge($driverAge);
 
-        return ($this->perKm * $km + $this->perHour * $hours) * $coefficent;
+        $result = ($this->perKm * $km + $this->perHour * $hours) * $coefficent;
+
+        if($gps){
+            $result = $this->addGps($result, $minute);
+        }
+
+        if($driver){
+            $result = $this->addDriver($result);
+        }
+
+        return $result;
 
     }
 }
@@ -59,6 +101,9 @@ class Day extends Rate
 {
     public $perKm = 1;
     public $perDay = 1000;
+
+    use Gps;
+    use Driver;
 
     public function checkTime($minute)
     {
@@ -71,13 +116,23 @@ class Day extends Rate
         }
     }
 
-    public function getPrice(int $km, int $minute, int $driverAge)
+    public function getPrice(int $km, int $minute, int $driverAge, bool $gps = false, bool $driver = false)
     {
-        $minute = $this->checkTime($minute);
+        $day = $this->checkTime($minute);
 
+        $coefficent = $this->checkAge($driverAge);
 
+        $result =  ($this->perKm * $km + $this->perDay * $day) * $coefficent;
 
-        return ($this->perKm * $km + $this->perDay * $minute);
+        if($gps){
+            $result = $this->addGps($result, $minute);
+        }
+
+        if($driver){
+            $result = $this->addDriver($result);
+        }
+
+        return $result;
     }
 }
 
@@ -86,9 +141,9 @@ class Student extends Rate
     public $perKm = 4;
     public $perMinute = 1;
 
+    use Gps;
 
-
-    public function getPrice(int $km, int $minute, int $driverAge)
+    public function getPrice(int $km, int $minute, int $driverAge, bool $gps = false)
     {
         if ($driverAge > 25){
             echo 'Тариф для водителей возрастом не болле 25 лет';
@@ -97,7 +152,13 @@ class Student extends Rate
 
         $coefficent = $this->checkAge($driverAge);
 
-        return ($this->perKm * $km + $this->perMinute * $minute) * $coefficent;
+        $result = ($this->perKm * $km + $this->perMinute * $minute) * $coefficent;
+
+        if($gps){
+            $result = $this->addGps($result, $minute);
+        }
+
+        return $result;
     }
 }
 
